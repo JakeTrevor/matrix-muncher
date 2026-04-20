62f304953609dfb17bffa0db76de22cbf9687709
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-04-20 17:12:49 +0100

    implemented permutation gate matrices

--- src/Permutation.hs
+++ src/Permutation.hs
@@ -1,15 +1,46 @@
--- module Permutation () where
+module Permutation (permGate, permGateInv) where
 
--- import Matrix
+import Data.List (delete, elemIndex)
+import Matrix
+import MatrixAlgebra
 
--- complete :: Int -> [Int] -> [Int]
--- complete x is = is ++ [j | j <- [1 .. x], (j `notElem` is)]
+complete :: Int -> [Int] -> [Int]
+complete x is = is ++ [j | j <- [1 .. x], (j `notElem` is)]
 
--- shiftRight l u = error "not implemented"
+swapGate :: MExpr
+swapGate =
+  MEVal $
+    Mat
+      [ [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1]
+      ]
 
--- gatePerm :: Int -> [Int] -> Mat
--- gatePerm x is =
---   let m = max is
---       idx = indexOf m is
---       rest = remove m is
---    in (kronecker (gatePerm (x - 1) rest) (idMat 2)) `matMul` shiftRight idx m
+idGate :: Int -> MExpr
+idGate x = MEVal $ idMat (2 ^ x)
+
+swapAdj :: Int -> Int -> MExpr
+swapAdj dim target = (idGate target) *^* swapGate *^* idGate (dim - target - 2)
+
+shiftRight :: Int -> Int -> MExpr
+shiftRight l dim = MEOp MatTimes [swapAdj dim i | i <- [l .. dim]]
+
+unsafeIndex :: (Eq a) => a -> [a] -> Int
+unsafeIndex x xs = case x `elemIndex` xs of
+  Nothing -> error "Could not find x in xs"
+  Just i -> i
+
+permGateInner :: Int -> [Int] -> MExpr
+permGateInner 1 [0] = MEVal $ idMat 2
+permGateInner x is =
+  let m = foldl1 max is
+      idx = m `unsafeIndex` is
+      rest = delete m is
+   in ((permGateInner (x - 1) rest) *^* (idGate 1)) * (shiftRight idx m)
+
+permGate :: Int -> [Int] -> Mat
+permGate x is = evalMExpr $ permGateInner x $ complete x is
+
+permGateInv :: Int -> [Int] -> Mat
+permGateInv x is = matTrans $ permGate x is
